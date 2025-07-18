1af7b90f4c4be635b117617da9621a26d95919a2
Ryan-Lucca <ryan.evangelista@picpay.com>
2025-07-18 11:05:58 -0300

    feat: adicionando padding lateral e arrumando estilo da nav

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,12 +5,11 @@ import { Outlet } from 'react-router-dom';
 export default function Layout() {
   return (
     <div className={styles.container}>
+      
       <div className={styles.content}>
         <Outlet />
       </div>
-      <nav className={styles.nav}>
-        <BottomNavBar />
-      </nav>
+      <BottomNavBar />
     </div>
   );
 }
